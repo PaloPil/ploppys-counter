fc4086fea77b6ceacb04ed1d8f6034181e2b259b
PaloPil <paul.rabste@gmail.com>
2024-02-23 12:23:10 +0100

    Beta 0.1
    Doesn't work, but allows me to share.

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
-import puppeteer from 'puppeteer';
+require('dotenv').config();
+const Discord = require('discord.js');
+const puppeteer = require('puppeteer');
 
 let url = 'https://www.draftbot.fr/economy/202859617917599745';
 
-(async () => {
+const client = new Discord.Client({
+  intents: 8,
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+});
+
+client.on('ready', () => {
+  client.user.setPresence({ activities: [{ name: '!money', type: Discord.ActivityType.Watching }], status: 'idle' });
+  console.log('Client connected as @' + client.user.tag);
+});
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.once("disconnect", () => client.error("Bot is disconnecting...", "warn"));
+client.once("reconnecting", () => client.warn("Bot reconnecting...", "log"));
+
+client.on('message', async message => {
+  console.log('Message received!');
+  if (message.content == '!money') {
+    console.log('Starting count!');
+    let money = await current_money();
+    message.reply('Il y a ' + money.toString() + ' Ploppy\'s en circulation dans le top 100.');
+  }
+});
+
+client.on('messageCreate', async message => {
+  console.log('msg');
+});
+
+
+async function current_money() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  
+
   await page.goto(url);
 
   let total_money = 0;
 
-  let content = '';
+  let top100 = await page.evaluate('() => Array.from(document.querySelectorAll("div.money span"), e => e.innerText)');
+
   for (let i = 0; i < 100; i++) {
-    content = await page.evaluate('document.querySelectorAll("div.money span")[' + str(i) + '].innerText');
-    if (content.includes("k")) {
-      total_money += 1000*float(content.replace("k",""));
+    if (top100[i].includes("k")) {
+      total_money += 1000*parseFloat(top100[i].replace("k",""));
     } else {
-      total_money += float(content);
+      total_money += parseFloat(top100[i]);
     }
-  };
+  }
 
   await browser.close();
 
-  console.log('Il y a actuellement ' + str(total_money) + ' Ploppy\'s en circulation dans le top 100.');
-  
-})();
+  return total_money;
+};
+
+client.login(process.env.BOT_TOKEN);
