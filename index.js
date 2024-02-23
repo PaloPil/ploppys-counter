56647c27a235cd3aeea8ad76de14f75072f316e7
Nivux <67231141+Niconnux@users.noreply.github.com>
2024-02-23 20:29:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('messageCreate', async message => {
   if (message.content == '!money') {
     console.log('Starting count!');
     try {
-      let money = await current_money();
+      let money = await current_money(message.channel);
       message.reply('Il y a ' + money.toString() + ' Ploppy\'s en circulation dans le top 100.');
     } catch (error) {
       message.reply(error)
@@ -43,17 +43,20 @@ client.on('messageCreate', async message => {
 });
 
 
-async function current_money() {
+async function current_money(channel) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(url);
+  channel.send('Début de la recherche')
 
   let total_money = 0;
 
   let top100 = await page.evaluate('() => Array.from(document.querySelectorAll("div.money span"), e => e.innerText)');
+  channel.send('Acces à la page')
 
   for (let i = 0; i < 100; i++) {
+    channel.send('Boucle ' + i)
     if (top100[i].includes("k")) {
       total_money += 1000*parseFloat(top100[i].replace("k",""));
     } else {
