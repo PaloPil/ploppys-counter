583354521610f99afd5bfb28460c1aeadad77753
PaloPil <paul.rabste@gmail.com>
2024-02-25 14:39:12 +0100

    Trying to install Chrome on Heroku

--- heroku-postbuild.bash
+++ heroku-postbuild.bash
@@ -4,4 +4,5 @@ if [ -d "/app/.cache/puppeteer" ]; then
     mv /app/.cache/puppeteer ./.cac
 else
     echo "/app/.cache/puppeteer does not exist, ignoring..."
+    npx puppeteer browsers install chrome
 fi
