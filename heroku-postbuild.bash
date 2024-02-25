007936c6f9422f1eca071abcf76d270a078356f6
PaloPil <paul.rabste@gmail.com>
2024-02-25 14:41:31 +0100

    Chrome installs, then I need to cache puppeteer

--- heroku-postbuild.bash
+++ heroku-postbuild.bash
@@ -1,8 +1,9 @@
 echo -e "Store puppeteer executable in cache\n"
 mkdir ./.cache
 if [ -d "/app/.cache/puppeteer" ]; then
-    mv /app/.cache/puppeteer ./.cac
+    mv /app/.cache/puppeteer ./.cache
 else
-    echo "/app/.cache/puppeteer does not exist, ignoring..."
+    echo "/app/.cache/puppeteer does not exist, installing Chrome..."
     npx puppeteer browsers install chrome
+    mv /app/.cache/puppeteer ./.cache
 fi
